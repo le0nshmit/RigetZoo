3947f20269ab89c66e5abf89ec82efb8e154317a
Leon Pickard-Smith <leonpickard6@gmail.com>
2026-02-04 20:56:31 +0000

    Registered booking blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,11 @@ def create_app():
     app = Flask(__name__)
 
     from .general import general_bp
+    from .booking import booking_bp
 
     app.register_blueprint(general_bp)
+    app.register_blueprint(booking_bp)
+
 
     return app
 
